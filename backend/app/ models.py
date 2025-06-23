15a65292a1e0a551b6358b44485f17236800f528
kiago <roy.kiago@student.moringaschool.com>
2025-06-23 15:09:06 +0300

    DEbug

--- backend/app/ models.py
+++ backend/app/ models.py
@@ -1,8 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
-db = SQLAlchemy()
-bcrypt = Bcrypt()
+from . import db, bcrypt
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
